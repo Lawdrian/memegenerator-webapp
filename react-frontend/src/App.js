3821a5e54ba3060aaa38ab74f66ad5826ac66567
Philip Burg <pburg@wlgore.com>
2023-12-18 14:42:57 +0100

    Added Routing and MaterialUI Theming with a Navbar

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+
+
+//pages
+import Home from './pages/Home';
+import Editor from './pages/Editor';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+
+//layouts
+import RootLayout from './layout/RootLayout';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>}>
+      <Route index element={<Home />} />
+      <Route path="editor" element={<Editor />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 

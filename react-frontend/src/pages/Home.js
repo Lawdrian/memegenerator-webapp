866551e9ee8c0f33ca7f6f0dcd358d861acdc5c5
Philip Burg <burguetta@gmail.com>
2024-01-09 18:51:39 +0100

    added simple endless scroll

--- react-frontend/src/pages/Home.js
+++ react-frontend/src/pages/Home.js
@@ -1,5 +1,36 @@
+import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import axios from "axios";
+import { set } from "lodash";
+
 export default function Home() {
-    return (
-        <h2>Home </h2>
-    )
+  const [memes, setMemes] = useState(Array.from({ length: 20 }));
+  const [hasMore, setHasMore] = useState(true);
+  const fetchMoreData = () => {
+    if (memes.length < 2000) {
+      setTimeout(() => {
+        setMemes(memes.concat(Array.from({ length: 20 })));
+      }, 1500);
+    } else {
+      setHasMore(false);
+    }
+  };
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <InfiniteScroll
+        dataLength={memes.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        
+      >
+        {memes.map((i, index) => (
+          <div style={{ height: 400 }} key={index}>
+            Image - #{index + 1}
+          </div>
+        ))}
+      </InfiniteScroll>
+    </div>
+  );
 }

182b2cd4e1f7eba8622ddb7d7da829cd93aced3a
Adrian <adrian@wild.network>
2024-02-21 20:41:33 +0100

    Fix general statistics

--- react-frontend/src/components/Statistics/StatisticCreatedMemes.jsx
+++ react-frontend/src/components/Statistics/StatisticCreatedMemes.jsx
@@ -1,49 +1,49 @@
 import { Grid } from '@mui/material';
 import React from 'react';
-import { useEffect } from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
 
-import { getAllMemes } from "../../api/meme";
 import { useState } from 'react';
 import { Button, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
 
-export default function StatisticCreatedMemes() {
+export default function GeneralStatistics() {
 
-  const [allMemes, setAllMemes] = useState([]);
   const [showStatistics, setStatistics] = useState([false]);
-
-
-  useEffect(() => {
-    getAllMemes(setAllMemes);
-    console.log(allMemes)
-    setStatistics(false)
-  }, [])
+  const currentYear = new Date().getFullYear()
+  const memes = useSelector((state) => state.meme.memes);
 
   function generateMemeData(){
-    if (!allMemes || allMemes?.length === 0) {
+    if (!memes || memes?.length === 0 || memes[0]?.createdAt === null) {
       console.log("no memes")
       return [];
     }
-    console.log("memes")
   //Collecting the statistic-data from the mems
-  const memeData = [];
   console.log("generateMemeData")
-  allMemes.forEach((meme) => {
-    const createdAt = new Date(meme.createdAt);
-    const month = createdAt.getMonth() + 1; //Month are 0-indexed
+  console.log(memes)
+  console.log(memes.length)
 
-    memeData[month] ? memeData[month]++ : (memeData[month] = 1);
-    console.log(memeData)
+
+  const dataSet = Array.from({ length: 12 }, () => 0);
+
+  memes.forEach((meme) => {
+    const createdAt = new Date(meme.createdAt);
+    if (createdAt.getFullYear() === currentYear) {
+    const month = createdAt.getMonth()
+    console.log("month", month)
+    dataSet[month] = (dataSet[month] || 0) + 1;
+    console.log(dataSet)
+    }
   });
-  console.log("memeData", memeData)
-  return memeData;
+
+  console.log("dataSet", dataSet)
+  return dataSet;
 };
   return (
 
     <Grid>
-      <Button onClick={() => showStatistics ? setStatistics(false) : setStatistics(true)}>Show Statistic: Memes created per Month</Button>
-      {(showStatistics && allMemes?.length != 0) && (
-        <Grid style={{ background: "#00FF00" }}>
+      <Button onClick={() => showStatistics ? setStatistics(false) : setStatistics(true)}>Show Statistic: Memes created per Month (all users)</Button>
+      {(showStatistics && memes?.length != 0 && memes[0]?.createdAt != null) && (
+        <Grid style={{backgroundColor: "#F5F5F5"}}>
           <Typography variant="h4" style={{ color: "green", textAlign: "center", marginBottom: 20 }}>Memes created per Month</Typography>
           <BarChart
             series={[{ data: generateMemeData() }]}
